9b49599fb85a97ef47123b88e34135c415de9cba
MaXRaY <2090943093@qq.com>
2026-04-27 11:14:10 +0800

    refactor: remove obsolete test scripts and temporary audio files

--- test_mvn.py
+++
@@ -1,8 +0,0 @@
-﻿import urllib.request
-url = 'https://search.maven.org/solrsearch/select?q=g:com.alibaba+AND+a:dashscope-sdk-java&rows=1&wt=json'
-req = urllib.request.Request(url)
-try:
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode())
-except Exception as e:
-    print('Error:', e)
